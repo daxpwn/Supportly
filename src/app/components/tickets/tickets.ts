5edac9d225e99743a0ae9ae271932110cef7f4d0
danijel-firmenfels <danijel.grebovic@firmenfels.ch>
2026-06-22 21:44:28 +0200

    Search

--- src/app/components/tickets/tickets.ts
+++ src/app/components/tickets/tickets.ts
@@ -1,9 +1,10 @@
-import { Component, afterNextRender, inject, signal } from '@angular/core';
+import { Component, afterNextRender, inject, signal, computed } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Ticket, TicketsService } from '../../services/tickets.service';
 
 @Component({
   selector: 'app-tickets',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './tickets.html',
 })
 export class Tickets {
@@ -13,19 +14,40 @@ export class Tickets {
   readonly tickets = signal<Ticket[]>([]);
   readonly loading = signal(true);
   readonly error = signal('');
-constructor() {
-  afterNextRender(() => {
-    this.ticketsService.getTickets().subscribe({
-      next: (data) => {
-        this.tickets.set(data);
-        this.loading.set(false);
-      },
-      error: (err) => {
-        this.error.set('Greška pri učitavanju tiketa.');
-        this.loading.set(false);
-        console.error(err);
-      },
-    });
+
+  searchTerm = signal('');
+
+  readonly filteredTickets = computed(() => {
+  const q = this.searchTerm().toLowerCase().trim();
+  const list = this.tickets();
+
+  if (!q) {
+    return list;
+  }
+
+  return list.filter((ticket) =>
+      ticket.subject.toLowerCase().includes(q) ||
+      ticket.ticketNumber.toLowerCase().includes(q) ||
+      ticket.status.toLowerCase().includes(q)
+    );
   });
-}
+
+
+  constructor() {
+    afterNextRender(() => {
+      this.ticketsService.getTickets().subscribe({
+        next: (data) => {
+          this.tickets.set(data);
+          this.loading.set(false);
+        },
+        error: (err) => {
+          this.error.set('Greška pri učitavanju tiketa.');
+          this.loading.set(false);
+          console.error(err);
+        },
+      });
+    });
+    
+  }
+  
 }
